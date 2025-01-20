9411af45a2afd6bd60ca3e4fd3c4ff5c73c4dbcd
jyzhang <peakfind@126.com>
2025-01-20 16:17:20 +0100

    complete

--- pml/pml-dc.jl
+++ pml/pml-dc.jl
@@ -11,6 +11,7 @@
 using Ferrite, FerriteGmsh
 using SparseArrays, StaticArrays, Arpack
 using Gmsh
+using Plots
 
 function coord_transform(x)
     # PML parameters
@@ -29,7 +30,6 @@ end
 
 """
     setup_grid(d, n, b, lc)
-    setup_grid(d, n, b, δ, lc, lp)
 
 Generate the mesh by julia interface for Gmsh and read the `.msh` file by `FerriteGmsh`
 
@@ -40,20 +40,9 @@ Generate the mesh by julia interface for Gmsh and read the `.msh` file by `Ferri
 - `b`: the height of th truncated domain
 - `lc`: target mesh size close to the point
 
-- `δ`: the width of the pml layer
-- `lp`: target mesh size in the pml layer
-
 # Points
 
 ```
-n+2 ------------- n+1
- |                 |
- 1  -------------  n
-```
-and
-```
-n+4 ------------- n+3
- |                 |
 n+2 ------------- n+1
  |                 |
  1  -------------  n
@@ -66,25 +55,12 @@ n+2 ------------- n+1
 n+2                n 
  .  -- 1 ~ n-1 --  .
 ```
-and
-```
- .  ---- n+4 ----  .
-n+5               n+3
- .  ---- n+1 ----  .
-n+2                n 
- .  -- 1 ~ n-1 --  .
-```
 
 # Examples
 
 ```julia
 d=2*pi, n=10, b=2
 setup_grid(2*pi, 10, 2)
-```
-
-```julia
-d=2*pi, n=10, b=2, δ=0.5
-setup_grid(2*pi, 20, 2, 0.5)
 ```
 
 # Misc 
@@ -130,7 +106,8 @@ function setup_grid(d, n, b, lc=0.5)
     # Create the Physical Groups
     gmsh.model.addPhysicalGroup(1, [lines[n+1]], -1, "Top")
     gmsh.model.addPhysicalGroup(1, [lines[n+2]], -1, "Left")
-    gmsh.model.addPhysicalGroup(1, [lines[n-1]], -1, "Right")
+    gmsh.model.addPhysicalGroup(1, [lines[n]], -1, "Right")
+    gmsh.model.addPhysicalGroup(1, lines[1:(n-1)], -1, "bottom")
     gmsh.model.addPhysicalGroup(2, [surf], -1, "Omega")
 
     # Set periodic mesh
@@ -140,78 +117,7 @@ function setup_grid(d, n, b, lc=0.5)
     # Generate a 2D mesh
     gmsh.model.mesh.generate(2)
     grid = mktempdir() do dir
-        path = joinpath(dir, "mesh.msh")
-        gmsh.write(path)
-        togrid(path) 
-    end
-
-    # Finalize the Gmsh library
-    gmsh.finalize()
-    return grid
-end
-
-function setup_grid(d, n, b, δ, lc=0.5, lp=0.1) 
-    h = d/(n-1)
-    # Initialize Gmsh
-    gmsh.initialize()
-    gmsh.option.setNumber("General.Verbosity", 2)
-
-    # Add the points
-    points = @MVector zeros(Int32, n+4)
-    
-    points[1] = gmsh.model.geo.addPoint(0, 0, 0, lc)
-    points[n] = gmsh.model.geo.addPoint(d, 0, 0, lc)
-    
-    for i in 2:n-1
-        points[i] = gmsh.model.geo.addPoint((i-1)*h, sin((i-1)*h), 0, lc) 
-    end
-
-    points[n+1] = gmsh.model.geo.addPoint(d, b, 0, lp)
-    points[n+2] = gmsh.model.geo.addPoint(0, b, 0, lp)
-    points[n+3] = gmsh.model.geo.addPoint(d, b+δ, 0, lp)
-    points[n+4] = gmsh.model.geo.addPoint(0, b+δ, 0, lp)
-
-    # Add the lines
-    lines = @MVector zeros(Int32, n+5)
-
-    for i in 1:n+1
-        lines[i] = gmsh.model.geo.addLine(points[i], points[i+1])
-    end
-
-    lines[n+2] = gmsh.model.geo.addLine(points[n+2], points[1])
-    lines[n+3] = gmsh.model.geo.addLine(points[n+1], points[n+3])
-    lines[n+4] = gmsh.model.geo.addLine(points[n+3], points[n+4])
-    lines[n+5] = gmsh.model.geo.addLine(points[n+4], points[n+2])
-
-    lb = gmsh.model.geo.addLine(points[n+4], points[1])
-    rb = gmsh.model.geo.addLine(points[n], points[n+3])
-
-    # Add the loop
-    lp1 = gmsh.model.geo.addCurveLoop(lines[1:n+2])
-    lp2 = gmsh.model.geo.addCurveLoop([-lines[n+1], lines[n+3], lines[n+4], lines[n+5]])
-
-    # Add the surface
-    domain = gmsh.model.geo.addPlaneSurface([lp1])
-    pml = gmsh.model.geo.addPlaneSurface([lp2])
-
-    # Synchronize the model
-    gmsh.model.geo.synchronize()
-
-    # Create the Physical Groups
-    gmsh.model.addPhysicalGroup(1, [lines[n+4]], -1, "Top")
-    gmsh.model.addPhysicalGroup(1, [lb], -1, "Left")
-    gmsh.model.addPhysicalGroup(1, [rb], -1, "Right")
-    gmsh.model.addPhysicalGroup(2, [domain, pml], -1, "Omega")
-
-    # Set periodic mesh
-    transform = @SVector [1, 0, 0, d, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
-    gmsh.model.mesh.setPeriodic(1, [rb], [lb], transform)
-
-    # Generate a 2D mesh
-    gmsh.model.mesh.generate(2)
-
-    grid = mktempdir() do dir
-        path = joinpath(dir, "mesh.msh")
+        path = joinpath(pwd(), "mesh.msh")
         gmsh.write(path)
         togrid(path) 
     end
@@ -234,11 +140,11 @@ function setup_dofs(grid::Grid, interpolation)
     return dh
 end
 
-function setup_bdcs(dofhandler::DofHandler)
+function setup_bdcs(dofhandler::DofHandler, d)
     csthandler = ConstraintHandler(dofhandler)
 
     # Set periodic boundary condition on the "Right" and "Left"
-    periodic_faces = collect_periodic_facets(dofhandler.grid, "Right", "Left", x -> x + Vec{2}((2*pi, 0.0)))
+    periodic_faces = collect_periodic_facets(dofhandler.grid, "Right", "Left", x -> x + Vec{2}((d, 0.0)))
     pbc = PeriodicDirichlet(:u, periodic_faces)
     add!(csthandler, pbc)
 
@@ -250,7 +156,7 @@ function setup_bdcs(dofhandler::DofHandler)
     return csthandler
 end
 
-function allocate_matries(dofhandler::DofHandler, csthandler::ConstraintHandler)
+function allocate_matrices(dofhandler::DofHandler, csthandler::ConstraintHandler)
     sp = init_sparsity_pattern(dofhandler)
     add_cell_entries!(sp, dofhandler)
     add_constraint_entries!(sp, csthandler)
@@ -282,7 +188,7 @@ by FEM and solve it by `Arpack`
 """
 function get_dispersion(cellvalues::CellValues, dofhandler::DofHandler, csthandler::ConstraintHandler, A::SparseMatrixCSC, B::SparseMatrixCSC, qms, neigs)
     m = length(qms)
-    dispersion = @MMatrix zeros(m, neigs)
+    dispersion = zeros(ComplexF64, m, neigs)
 
     # Assemble the matrix B
     B = assemble_b(cellvalues, B, dofhandler)
@@ -295,8 +201,8 @@ function get_dispersion(cellvalues::CellValues, dofhandler::DofHandler, csthandl
         apply!(A, csthandler)
 
         # solve the general eigenvalue problem
-        λ, _ = eigs(A, B, nev = neigs, which = :LM)
-        disperion[i, :] = λ
+        λ, _ = eigs(A, B, nev = neigs, which = :SM)
+        dispersion[i, :] = λ
         fill!(A, 0.0)
     end
     
@@ -306,7 +212,7 @@ end
 function assemble_a(cellvalues::CellValues, A::SparseMatrixCSC, dofhandler::DofHandler, qm)
     ndofs_c = ndofs_per_cell(dofhandler)
     assembler = start_assemble(A)
-    coef = @MMatrix zeros(2, 2)
+    coef = @MMatrix zeros(ComplexF64, 2, 2)
     
     # Preallocate the local stiffness matrix
     Ae = zeros(ComplexF64, ndofs_c, ndofs_c)
@@ -315,6 +221,7 @@ function assemble_a(cellvalues::CellValues, A::SparseMatrixCSC, dofhandler::DofH
         # Reinitialize cellvalues for this cell
         reinit!(cellvalues, cell)
         fill!(Ae, 0)
+        coords = getcoordinates(cell)
 
         for qp in 1:getnquadpoints(cellvalues)
             dx = getdetJdV(cellvalues, qp)
@@ -385,22 +292,21 @@ function main()
 
     cv = setup_fevs(ip)
     dh = setup_dofs(grid, ip)
-    ch = setup_bdcs(dh)
+    ch = setup_bdcs(dh, d)
 
     # Allocate the matrices
     A = allocate_matrices(dh, ch)
     B = allocate_matrices(dh, ch)
 
     # Brillouin zone α
-    α = collect(range(-pi/d, pi/d, 10))
+    α = collect(range(-pi/d, pi/d, 80))
 
     #
     k = get_dispersion(cv, dh, ch, A, B, α, 5)
 
     # Plot the dispersion curves
-    @show k
-
-    return nothing
+    # plot(α, real(k), legend=false)
+    plot(α, imag(k), legend=false)
 end
 
 main()
