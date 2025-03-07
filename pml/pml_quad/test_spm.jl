afb6660c2ecffcc25dc16b90def66b93bf0c9fa4
jyzhang <peakfind@126.com>
2025-03-07 15:12:18 +0100

    driver for pml_quad.jl

---
+++ pml/pml_quad/test_spm.jl
@@ -0,0 +1,42 @@
+using Gmsh
+using Ferrite, FerriteGmsh
+using SparseArrays
+using NonlinearEigenproblems
+using Plots
+
+include("pml_quad.jl")
+
+d = 2π
+grid = setup_grid(;d=2π, h=1.5, δ=0.5, lc=0.5)
+
+# Define the interpolation: linear lagrange
+ip = Lagrange{RefTriangle, 1}()
+
+# Setup FE values, dofs, and boundary conditions
+cv = setup_fevs(ip)
+dh = setup_dofs(grid, ip)
+ch = setup_bdcs(dh, d)
+
+# Allocate the matrices
+f₀ = zeros(ComplexF64, ndofs(dh))
+f₁ = zeros(ComplexF64, ndofs(dh))
+f₂ = zeros(ComplexF64, ndofs(dh))
+A₀ = allocate_matrices(dh, ch)
+A₁ = allocate_matrices(dh, ch)
+A₂ = allocate_matrices(dh, ch)
+
+A₀ = assemble_A0(cv, dh, A₀)
+A₁ = assemble_A1(cv, dh, A₁)
+A₂ = assemble_A0(cv, dh, A₂)
+# apply!(A₀, f, ch)
+# apply!(A₁, f, ch)
+# apply!(A₂, f, ch)
+apply!(A₀, f₀, ch)
+apply!(A₁, f₁, ch)
+apply!(A₂, f₂, ch)
+
+# Set NEP 
+qep = PEP([A₀, A₁, A₂])
+λ,_ = polyeig(qep)
+# filter!(e -> is_real(e), λ)
+@show λ
