3a7953cb6a49b3d687ec342435d8be604431039d
jyzhang <peakfind@126.com>
2025-04-02 13:26:51 +0200

    driver for flat surface

--- dtn/driver_dtn.jl
+++ dtn/driver_dtn.jl
@@ -1,3 +1,7 @@
+# Test the code snippets for DtN-FEM in dtn.jl
+# An flat surface illuminated by an plane wave with 
+# Dirichlet boundary condition.
+
 using Ferrite
 using SparseArrays
 using Gmsh
@@ -12,13 +16,13 @@ k = 1.0
 inc = Incident(k, θ)
 
 # Number of the truncated terms
-N = 15
+N = 10
 height = 2.0
 d = 2π
 
 # Generate a uniform mesh
-# grid = periodic_cell(0.1; height=height)
-grid = generate_grid(Triangle, (10, 10), Vec{2}((0.0, 0.0)), Vec{2}((d, 0.0)), Vec{2}((d, height)), Vec{2}((0.0, height)))
+grid = periodic_cell(0.1; height=height)
+# grid = generate_grid(Triangle, (40, 40), Vec{2}((0.0, 0.0)), Vec{2}((d, 0.0)), Vec{2}((d, height)), Vec{2}((0.0, height)))
 
 ## Set up fevalues(CellValues and FacetValues), DofHandler, and ConstraintHandler
 ip = Lagrange{RefTriangle, 1}() # Interpolation and quadrature rules
@@ -44,8 +48,10 @@ f = assemble_load(fv, dh, getfacetset(grid, "top"), f, inc, height)
 # Assemble the TBC matrix
 F = assemble_tbc(fv, dh, inc, getfacetset(grid, "top"), F, N, dofsDtN)
 
+A = sub_preserve_structure(A, F)
+
 # Get the global stiffness matrix by A - F
-A .-= F
+# A .-= F
 
 # Impose the boundary condition
 apply!(A, f, cst)
@@ -56,7 +62,7 @@ apply!(u, cst)
 
 # Write the solution to vtk file
 VTKGridFile("real_u", grid) do vtk
-    write_solution(vtk, dh, real.(u)) 
+   write_solution(vtk, dh, real.(u)) 
 end;
 
 VTKGridFile("imag_u", grid) do vtk
